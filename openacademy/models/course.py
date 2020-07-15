74c0f93b119cfc497ebfa6dc8501cc83a6e24733
Lesteraft <40004561+Lesteraft@users.noreply.github.com>
2020-07-15 15:02:29 -0600

    agregar form4

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -1,9 +1,11 @@
 from odoo import models, fields, api
 
 class Course(models.Model):
+    _name = 'openacademy.course'
+    _description = "OpenAcademy Courses"
+
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
-
 
 class Session(models.Model):
     _name = 'openacademy.session'
