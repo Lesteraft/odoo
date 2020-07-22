caaf0bfb20cba1b7b0aa8f056457d289a1a89933
Lester <lesteraft@gmail.com>
2020-07-22 22:25:01 +0000

    [ADD] Reports - Printed reports

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -30,6 +30,7 @@
         # '/views/views.xml',
         'views/openacademy.xml',
         'views/partner.xml',
+        'reports.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
