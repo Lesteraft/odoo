13d102fe0e43e1745b1a00a0c09c7df43c8a11c6
Lesteraft <40004561+Lesteraft@users.noreply.github.com>
2020-07-21 23:20:53 -0600

    WIZARD

--- openacademy/wizard.py
+++ openacademy/wizard.py
@@ -6,13 +6,6 @@ class Wizard(models.TransientModel):
     _name = 'openacademy.wizard'
     _description = "Wizard: Quick Registration of Attendees to Sessions"
 
-    def _default_session(self):
-        return self.env['openacademy.session'].browse(self._context.get('active_id'))
-
     session_id = fields.Many2one('openacademy.session',
-        string="Session", required=True, default=_default_session)
+        string="Session", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
-
-    def subscribe(self):
-        self.session_id.attendee_ids |= self.attendee_ids
-        return {}
